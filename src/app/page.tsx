c27a3f0fafae4c2fc0379428b1d0d2194a98430d
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-07 19:59:13 +0300

    Used new layout. Add self-center to container.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,14 @@ import WhyUs from '@/components/WhyUs'
 
 export default function Home() {
   return (
-    <main className='flex flex-col justify-center'>
-        <div className='mx-auto'>
-            <div className='max-w-[650px] m-[30px] rounded-md overflow-hidden'>
+    <main className='relative flex flex-col justify-center mx-auto'>
+            <div className='max-w-[650px] m-[30px] rounded-md overflow-hidden self-center'>
                 <JoinCommunity />
                 <div className='flex lg:flex-row flex-col w-auto'>
                     <MonthlySubscription />
                     <WhyUs />
                 </div>
             </div>
-        </div>
     </main>
   )
 }

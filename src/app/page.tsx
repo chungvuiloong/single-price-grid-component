65226f20fc4f145e6b3d41b2394ad684e5aee318
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-07 22:22:16 +0300

    Added shadow to box.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import WhyUs from '@/components/WhyUs'
 
 export default function Home() {
   return (
-    <main className='lg:h-[100vh] h-auto relative flex flex-col justify-center mx-auto'>
+    <main className='lg:h-[100vh] h-auto relative flex flex-col justify-center mx-auto shadow-md shadow-inner'>
         <div className='max-w-[650px] m-[30px] rounded-md overflow-hidden self-center'>
             <JoinCommunity />
             <div className='flex lg:flex-row flex-col w-auto'>

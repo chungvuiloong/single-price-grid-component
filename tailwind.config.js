965efdd4fae6c7e32c2493ff865d3270417696f3
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-07 13:01:25 +0300

    Added custom color to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,14 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'Cyan': 'hsl(179, 62%, 43%)',
+        'Bright-Yellow': 'hsl(71, 73%, 54%)',
+        'Light-Gray': 'hsl(204, 43%, 93%)',
+        'Grayish-Blue': 'hsl(218, 22%, 67%)',
+      },
+    },
   },
   plugins: [require("daisyui")],
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,15 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'Cyan': 'hsl(179, 62%, 43%)',
+      'Bright-Yellow': 'hsl(71, 73%, 54%)',
+      'Light-Gray': 'hsl(204, 43%, 93%)',
+      'Grayish-Blue': 'hsl(218, 22%, 67%)',
+    },
+    extend: {
+
+    }
   },
   plugins: [require("daisyui")],
 }

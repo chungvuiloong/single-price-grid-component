8f18bb19de4c97aca88fe34f39e2970ff01f5574
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-07 20:16:20 +0300

    Prettied code.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,14 @@ import WhyUs from '@/components/WhyUs'
 export default function Home() {
   return (
     <main className='lg:h-[100vh] h-auto relative flex flex-col justify-center mx-auto'>
-            <div className='max-w-[650px] m-[30px] rounded-md overflow-hidden self-center'>
-                <JoinCommunity />
-                <div className='flex lg:flex-row flex-col w-auto'>
-                    <MonthlySubscription />
-                    <WhyUs />
-                </div>
+        <div className='max-w-[650px] m-[30px] rounded-md overflow-hidden self-center'>
+            <JoinCommunity />
+            <div className='flex lg:flex-row flex-col w-auto'>
+                <MonthlySubscription />
+                <WhyUs />
             </div>
-            <Footer />
+        </div>
+        <Footer />
     </main>
   )
 }

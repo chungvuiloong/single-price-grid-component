85f73651df13560025639b45dc202b9b80d88adf
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-07 19:13:49 +0300

    Centered the content.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,14 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className='h-full'>
-        <div className='max-w-[650px]'>
-            <JoinCommunity />
-            <div className='flex lg:flex-row flex-col w-auto'>
-                <MonthlySubscription />
-                <WhyUs />
+    <main className='h-[100vh] flex flex-col justify-center'>
+        <div className='mx-auto'>
+            <div className='max-w-[650px]'>
+                <JoinCommunity />
+                <div className='flex lg:flex-row flex-col w-auto'>
+                    <MonthlySubscription />
+                    <WhyUs />
+                </div>
             </div>
         </div>
     </main>

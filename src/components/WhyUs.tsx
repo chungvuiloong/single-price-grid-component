663228bb4ad3320bce1a5e3124119e8b9d527e15
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-07 20:17:51 +0300

    Increased the brightness.

--- src/components/WhyUs.tsx
+++ src/components/WhyUs.tsx
@@ -1,6 +1,6 @@
 const WhyUs = () => {
     return (
-        <div className='basis-1/2 flex flex-col gap-3 bg-Cyan brightness-105 lg:p-10 p-7 text-white'>
+        <div className='basis-1/2 flex flex-col gap-3 bg-Cyan brightness-110 lg:p-10 p-7 text-white'>
             <h2 className='text-lg'>Why us</h2>
             <p className='text-Light-Gray opacity-80 text-sm'>
             Tutorials by industry experts <br/>

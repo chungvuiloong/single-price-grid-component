3781b9f3968a680bb8f738fb7de1fd2aa2088ba3
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-07 18:24:26 +0300

    Made the height full page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main>
+    <main className='h-[100vw]'>
         <JoinCommunity />
         <div className='flex lg:flex-row flex-col'>
             <MonthlySubscription />

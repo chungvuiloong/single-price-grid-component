d9e758388013852c8462b45c81b0c33ff8b3d961
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-08 08:33:28 +0300

    Changed p tag to ul and children as li.

--- src/components/WhyUs.tsx
+++ src/components/WhyUs.tsx
@@ -2,15 +2,15 @@ const WhyUs = () => {
     return (
         <div className='basis-1/2 flex flex-col gap-3 bg-Cyan brightness-110 lg:p-10 p-7 text-white'>
             <h2 className='text-lg'>Why us</h2>
-            <p className='text-Light-Gray opacity-80 text-sm'>
-            Tutorials by industry experts <br/>
-            Peer &amp; expert code review <br/>
-            Coding exercises <br/>
-            Access to our GitHub repos <br/>
-            Community forum <br/>
-            Flashcard decks <br/>
-            New videos every week <br/>
-            </p>
+            <ul className='text-Light-Gray opacity-80 text-sm'>
+                <li>Tutorials by industry experts</li>
+                <li>Peer &amp; expert code review</li>
+                <li>Coding exercises</li>
+                <li>Access to our GitHub repos</li>
+                <li>Community forum</li>
+                <li>Flashcard decks</li>
+                <li>New videos every week</li>
+            </ul>
         </div>
     );
 };
